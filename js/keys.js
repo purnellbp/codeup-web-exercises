638e85ed179d639e1b4c588d0a17547fb1e87b4c
Brian Purnell <purnellbp@gmail.com>
2020-03-04 14:52:05 -0600

    added key

--- js/keys.js
+++ js/keys.js
@@ -1,4 +1,5 @@
 
 "use strict";
 var mapboxToken = "pk.eyJ1IjoicHVybmVsbGJwIiwiYSI6ImNrNnV4d2dtbDBleHAzZnBudW14Z2VweGsifQ.TcGUbeIR8s-vagfNSK7x4w";
+var darkSkysKey = "367c5d9973a93e063155e375c4640d28";
 
