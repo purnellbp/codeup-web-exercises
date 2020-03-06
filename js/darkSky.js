a2f741e9c10537d94a21e1043767840b725f8941
Brian Purnell <purnellbp@gmail.com>
2020-03-06 00:01:21 -0600

    updated refresh function. added icons.

--- js/darkSky.js
+++ js/darkSky.js
@@ -1,62 +1,106 @@
 "use strict";
-
+// TODO: Interactive Hourly
+// TODO: Pollen Count
+// TODO: Add Charts https://canvasjs.com/javascript-charts/chart-image-overlay
+// IMPORTANT - Does the initial pull for the weather to populate the document.
 $(document).ready(function () {
-    pullWeatherUS();
-});
+   pullWeatherUS(0);
+    setTimeout(function(){
+        pullWeatherUS(1);
+         }, 1000);
+    setTimeout(function(){
+    pullWeatherUS(2);
+    }, 1000);
 
+});
+// *************************************************************************************************
+
+// TODO: Map Navigation
 // MapBod Style URL
 const mapStyleUrl = "mapbox://styles/purnellbp/ck7ez41uo02q21io1eh6xpbzy";
 // Call dark sky api
 var lat = 29.314;
 var lng = -98.348;
-var weatherURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/367c5d9973a93e063155e375c4640d28/"
-    + lat
-    + ","
-    + lng;
-// Object table build. To easlily access data later
-var iconTable = {
+// var weatherURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/367c5d9973a93e063155e375c4640d28/"
+//     + lat
+//     + ","
+//     + lng;
 
-    "time": 1583388000,
-    "summary": "Clear throughout the day.",
-    "icon": "clear-day",
-    "sunriseTime": 1583412900,
-    "sunsetTime": 1583455020,
-    "moonPhase": 0.36,
-    "precipIntensity": 0.0008,
-    "precipIntensityMax": 0.0023,
-    "precipIntensityMaxTime": 1583472180,
-    "precipProbability": 0.06,
-    "precipType": "rain",
-    "temperatureHigh": 75.87,
-    "temperatureHighTime": 1583446380,
-    "temperatureLow": 50.67,
-    "temperatureLowTime": 1583495820,
-    "apparentTemperatureHigh": 75.37,
-    "apparentTemperatureHighTime": 1583446380,
-    "apparentTemperatureLow": 51.16,
-    "apparentTemperatureLowTime": 1583495820,
-    "dewPoint": 42.1,
-    "humidity": 0.53,
-    "pressure": 1023.7,
-    "windSpeed": 8.62,
-    "windGust": 28.2,
-    "windGustTime": 1583388000,
-    "windBearing": 0,
-    "cloudCover": 0.21,
-    "uvIndex": 8,
-    "uvIndexTime": 1583433900,
-    "visibility": 10,
-    "ozone": 294.3,
-    "temperatureMin": 49.94,
-    "temperatureMinTime": 1583407860,
-    "temperatureMax": 75.87,
-    "temperatureMaxTime": 1583446380,
-    "apparentTemperatureMin": 50.43,
-    "apparentTemperatureMinTime": 1583407860,
-    "apparentTemperatureMax": 75.37,
-    "apparentTemperatureMaxTime": 1583446380
+function urlBuilder(lat, lng) {
+    return "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkysKey + "/" + lat + "," + lng;
+}
 
-};
+// ****************************************************************************************************
+// ************* UTILITIES ****************************************************************************
+
+// Converts the data.icon element to the appropriate class name to display the proper weather icon.
+// Icon/font pack borrowed from https://erikflowers.github.io/weather-icons/
+// clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, partly-cloudy-night
+function iconPull(iconName) {
+    var output = "";
+    switch (iconName) {
+        case 'clear-day':
+            output = "wi-forecast-io-clear-day";
+            break;
+        case 'clear-night':
+            output = "wi-forecast-io-clear-night";
+            break;
+        case  'rain':
+            output = "wi-forecast-io-rain";
+            break;
+        case  'snow':
+            output = "wi-forecast-io-snow";
+            break;
+        case 'sleet':
+            output = "wi-forecast-io-sleet";
+            break;
+        case 'strong-wind':
+            output = "wi-forecast-io-wind";
+            break;
+        case 'fog':
+            output = "wi-forecast-io-fog";
+            break;
+        case 'cloudy':
+            output = "wi-forecast-io-cloudy";
+            break;
+        case 'day-cloudy':
+            output = "wi-forecast-io-partly-cloudy-day";
+            break;
+        case 'partly-cloudy-day':
+            output = "wi-forecast-io-partly-cloudy-day";
+            break;
+        case 'night-cloudy':
+            output = "wi-forecast-io-partly-cloudy-night";
+            break;
+        case 'hail':
+            output = "wi-forecast-io-hail";
+            break;
+        case 'thunderstorm':
+            output = "wi-forecast-io-thunderstorm";
+            break;
+        case 'tornado':
+            output = "wi-forecast-io-tornado";
+            break;
+        case 'default':
+            output = "d-none";
+            break;
+    }
+    return "<i class=\"wi " + output + "\"></i>";
+}
+
+// *******************************************************
+// Responsive settings
+$(window).resize(function () {
+    if ($(window).width() > 1200) {
+        $('.wi').css('font-size', '5rem')
+    } else {
+        $('.wi').css('font-size', '2.5rem')
+    }
+});
+var what = $(window).width();
+console.log("Current screen width is " + what);
+// *******************************************************
+
 
 // Converts UNIX Time Stamp to Human readable
 function timeConvert(unixTime) {
@@ -64,64 +108,116 @@ function timeConvert(unixTime) {
     return dateObject.toLocaleDateString();
 }
 
-
-// **************************************
-function fToC(fahrenheit)
-{
+// Converts fahrenheit to celsius
+function fToC(fahrenheit) {
     var fTemp = fahrenheit;
     var fToCel = (fTemp - 32) * 5 / 9;
-    var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    var message = fTemp + '\xB0F is ' + fToCel + '\xB0C.';
     return fToCel.toFixed(0);
 }
 
+// Converts angle to cardinal direction
+function getCardinal(angle) {
+    var degreePerDirection = 360 / 8;
+    const offsetAngle = angle + degreePerDirection / 2;
+    return (offsetAngle >= 0 * degreePerDirection && offsetAngle < 1 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-n\"></i><span>N</span>"
+        : (offsetAngle >= 1 * degreePerDirection && offsetAngle < 2 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-ne\"></i><span>NE</span>"
+            : (offsetAngle >= 2 * degreePerDirection && offsetAngle < 3 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-e\"></i><span>E</span>"
+                : (offsetAngle >= 3 * degreePerDirection && offsetAngle < 4 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-se\"></i><span>N</span>"
+                    : (offsetAngle >= 4 * degreePerDirection && offsetAngle < 5 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-s\"></i><span>S</span>"
+                        : (offsetAngle >= 5 * degreePerDirection && offsetAngle < 6 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-sw\"></i><span>SW</span>"
+                            : (offsetAngle >= 6 * degreePerDirection && offsetAngle < 7 * degreePerDirection) ? "<i class=\"wi wi-wind wi-from-w\"></i><span>W</span>"
+                                : "<i class=\"wi wi-wind wi-from-nw\"></i><span>NW</span>";
+}
 
-// **************************************
-
-
-$("#mericanTemp").click(function(){
-    pullWeatherUS();
+// ******************************************************************************************************
+// ************* LISTENERS ******************************************************************************
+// If the Fahrenheit option is selected we pull the standard API results
+$("#mericanTemp").click(function () {
+    pullWeatherUS(0);
+    setTimeout(function(){
+        pullWeatherUS(1);
+    }, 1000);
+    setTimeout(function(){
+        pullWeatherUS(2);
+    }, 1000);
 });
+// If metric is selected we convert temperatures and wind to celsius and kilometers
 $("#metricTemp").click(function () {
-    pullWeatherMetric();
+    pullWeatherMetric(0);
+    pullWeatherMetric(0);
+    setTimeout(function(){
+        pullWeatherMetric(1);
+    }, 1000);
+    setTimeout(function(){
+        pullWeatherMetric(2);
+    }, 1000);
+
 });
 
-function getMetaData() {
-    // DAILY temp high and low[x].. icon[ ] .. clouds:[ ] .. humidity[ ].. wind speed[ ] .. pressure[ ] ..
 
-    $.get(weatherURL).done(function (data) {
+// ******************************************************************************************************
+// ************* WEATHER BUILD **************************************************************************
+
+// TODAY'S FORECAST - [0]
+// Gets all the appropriate data to display the weather forecasts, adjust styling responsively.
+// DAILY temp high and low[x].. icon[ ] .. clouds:[ ] .. humidity[ ].. wind speed[ ] .. pressure[ ] ..
+function getMetaData() {
+
+    $.get(urlBuilder(lat, lng)).done(function (data) {
         var tmpTime = data.daily.data[0].time;
         $('#todayDate').empty().append(timeConvert(tmpTime))
-        $('#dayZero-weatherBox').append("<br>"+data.daily.data[0].icon)
-
+        if ($(window).width() > 1200) {
+            $('.wi').css('font-size', '3rem',);
+        }
 
     });
 }
 
-function pullWeatherUS() {
-    $.get(weatherURL).done(function (data) {
-        console.log(data.daily.data[0]);
-        $('#dayZero-weatherBox').empty().append(data.daily.data[0].temperatureHigh.toFixed(0) + "|" + data.daily.data[0].temperatureLow.toFixed(0) +
-            "<span>&#8457</span>"
-        )
+// This is the Standard get without conversions then refreshes the box
+// TODO: make the weather api call and DOM all one funciton !!!!
+// TODO: make the weather api call and DOM all one funciton !!!!
+// TODO: make the weather api call and DOM all one funciton !!!!
+// TODO: make the weather api call and DOM all one funciton !!!!
+
+function pullWeatherUS(i) {
+    var classDay = "#" + i + "-weatherBox";
+    $.get(urlBuilder(lat, lng)).done(function (data) {
+        console.log(data.daily.data[i]);
+        $(classDay).empty().append(data.daily.data[i].temperatureHigh.toFixed(0) + " | " +
+        data.daily.data[i].temperatureLow.toFixed(0) +
+        "<span>&#8457</span>" +
+        "<br>" +
+        iconPull(data.daily.data[i].icon) +
+        "<br>" +
+        getCardinal(data.daily.data[i].windbearing)
+            );
     });
     getMetaData();
 }
 
-function pullWeatherMetric(){
-    $.get(weatherURL).done(function (data) {
-        var hi = data.daily.data[0].temperatureHigh;
-        var low = data.daily.data[0].temperatureLow;
+// This calls the same as standard but with some fahrenheit to celsius conversions then refreshes the box
+function pullWeatherMetric(i) {
+    var classDay = "#" + i + "-weatherBox";
+    $.get(urlBuilder(lat, lng)).done(function (data) {
+        var hi = data.daily.data[i].temperatureHigh;
+        var low = data.daily.data[i].temperatureLow;
+        var windBear = data.daily.data[i].windbearing;
         var tempInCelsiusHi = fToC(hi);
         var tempInCelsiusLow = fToC(low);
-        console.log(tempInCelsiusHi);
-        console.log(tempInCelsiusLow);
-        $('#dayZero-weatherBox').empty().append( tempInCelsiusHi + "|" + tempInCelsiusLow +
-            "<span>\xB0C</span>"
-        )
+        $('#dayZero-weatherBox').empty().append(tempInCelsiusHi + " | " + tempInCelsiusLow + "<span>\xB0C</span>" +
+        "<br>" +
+        iconPull(data.daily.data[i].icon) +
+        "<br>" +
+        getCardinal(windBear)
+            );
     });
     getMetaData();
 }
-
+// TODO: make the weather api call and DOM all one funciton !!!!
+// TODO: make the weather api call and DOM all one funciton !!!!
+// TODO: make the weather api call and DOM all one funciton !!!!
+// TODO: make the weather api call and DOM all one funciton !!!!
 
 
 // Sandboxing
