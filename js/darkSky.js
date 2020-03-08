cf6f4ec7631bcbde85a282f0795a1709c2d427e2
Brian Purnell <purnellbp@gmail.com>
2020-03-08 05:37:02 -0500

    updated weather app

--- js/darkSky.js
+++ js/darkSky.js
@@ -2,147 +2,176 @@
 // TODO: Interactive Hourly
 // TODO: Pollen Count
 // TODO: Add Charts https://canvasjs.com/javascript-charts/chart-image-overlay
+// TODO:
 // IMPORTANT - Does the initial pull for the weather to populate the document.
-$(document).ready(function () {
-   pullWeatherUS(0);
-    setTimeout(function(){
-        pullWeatherUS(1);
-         }, 1000);
-    setTimeout(function(){
-    pullWeatherUS(2);
-    }, 1000);
-
+$('document').ready(function () {
+    // Start scripts
+    loadMapbox();
+    setTimeout(function () {
+        lottie.setSpeed(0.5, 'img/weather-img/data.json')
+        lottie.loadAnimation({
+            container: document.getElementById('logo'),
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+            playSpeed: 0.2,
+            path: 'img/weather-img/data.json' // the path to the animation json
+        });
+    }, 2000);
 });
 // *************************************************************************************************
 
-// TODO: Map Navigation
-// MapBod Style URL
-const mapStyleUrl = "mapbox://styles/purnellbp/ck7ez41uo02q21io1eh6xpbzy";
+
 // Call dark sky api
-var lat = 29.314;
-var lng = -98.348;
-// var weatherURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/367c5d9973a93e063155e375c4640d28/"
-//     + lat
-//     + ","
-//     + lng;
+var lat = 29.4383;
+var lng = -98.5031;
+var darkURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkysKey + "/" + lat + "," + lng;
+var tempSwitch = 0;
+// Forecast cards
+function buildCards() {
 
-function urlBuilder(lat, lng) {
-    return "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkysKey + "/" + lat + "," + lng;
-}
-// *************************************************************************************************
-// GEOLOCATION Data
-// *************************************************************************************************
-// location Search url
-// 'https://api.mapbox.com/geocoding/v5/mapbox.places/san%20anto.json?access_token=pk.eyJ1IjoibWF0dGZpY2tlIiwiYSI6ImNqNnM2YmFoNzAwcTMzM214NTB1NHdwbnoifQ.Or19S7KmYPHW8YjRz82v6g'
+    $.get(darkURL)
+        .done(function (data) {
+            console.log(data);
+            // Build Start
+            for (var i = 0; i < data.daily.data.length; i++) {
+                var dayA = data.daily.data[i];
+                var thisCard = "#day" + i;
+                var tempHigh = dayA.temperatureHigh + "\xB0F";
+                var tempLow = dayA.temperatureLow + "\xB0F";
+                if (tempSwitch === 1) {
+                    tempHigh = fToC(dayA.temperatureHigh) + "\xB0C";
+                    tempLow = fToC(dayA.temperatureLow) + "\xB0C";
+                }
+                $(thisCard).append("<h5 class=\"card-title\">" + timeConvert(dayA.time))
+                    .append("</h5><p class=\"card-text\">" + dayA.summary + "</p>") // "blah blah throughout the day"
+                    .append("<ul class=\"list-group list-group-flush\">")
+                    .append("<li class=\"list-group-item text-primary\">" + tempHigh + " | " + tempLow + "</li>")
+                    .append("<li class=\"list-group-item\">" + iconPull(dayA.icon) + "|" + getCardinal(dayA.windBearing) + "</li>")
+                    .append("<li class=\"list-group-item\">Ves</li></ul>")
 
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-     $.get(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
-        // .then(function(res) {
-        //     return res.json();
-        //     // to get all the data from the request, comment out the following three lines...
-        // })
-        .done(function(data) {
-            // console.log("fucntion returned: ");
-            // console.log(data.features[0].center);
-            return data.features[0].center;
 
-            // console.log(data);
+            }
         });
 }
 
-
-$(document).on('keypress',function(e) {
-    if(e.which === 13) {
-        var search  = $('#searchInput').val();
-        console.log("user entered: " + search);
-        geocode(search, mapboxToken);
-        console.log(newCenter);
-        // Fly to a random location by offsetting the point -74.50, 40
-// by up to 5 degrees.
-        map.flyTo({
-            center: [0,0],
-            essential: true // this animation is considered essential with respect to prefers-reduced-motion
-        });
-    }
-});
+buildCards();
 // ****************************************************************************************************
 // MapBox
 // ****************************************************************************************************
-mapboxgl.accessToken = mapboxToken;
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-98.4951, 29.4246],
-    zoom: 10,
-    pitch: 60, // pitch in degrees
-    bearing: 0 // bearing in degrees
-});
+var mapStyleUrl = "mapbox://styles/purnellbp/ck7i2q7hr1qpy1ik7z9rg3a08";
 
+function loadMapbox() {
+    mapboxgl.accessToken = mapboxToken;
+    var coordinates = document.getElementById('coordinates');
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: mapStyleUrl,
+        center: [0, 0],
+        zoom: 2,
+        pitch: 30
+    });
+
+    map.addControl(
+        new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            mapboxgl: mapboxgl
+        })
+        );
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([0, 0])
+        .addTo(map);
+
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        console.log(lngLat);
+        coordinates.style.display = 'block';
+        coordinates.innerHTML =
+            'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+    }
+
+    marker.on('dragend', onDragEnd);
+};
 // ****************************************************************************************************
 // ************* UTILITIES ****************************************************************************
-
 // Converts the data.icon element to the appropriate class name to display the proper weather icon.
 // Icon/font pack borrowed from https://erikflowers.github.io/weather-icons/
 // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, partly-cloudy-night
 function iconPull(iconName) {
     var output = "";
+    var clouds = "";
     switch (iconName) {
         case 'clear-day':
             output = "wi-forecast-io-clear-day";
+            clouds = " It's clear today!";
             break;
         case 'clear-night':
             output = "wi-forecast-io-clear-night";
+            clouds = " Clear night.";
             break;
         case  'rain':
             output = "wi-forecast-io-rain";
+            clouds = " <b>Rain</b> today";
             break;
         case  'snow':
-            output = "wi-forecast-io-snow";
+            output = "wi-forecast-io-snow text-warning";
+            clouds = " <b>Snow</b> today.";
             break;
         case 'sleet':
-            output = "wi-forecast-io-sleet";
+            output = "wi-forecast-io-sleet  text-danger";
+            clouds = " <b>Rain:</b> Sleet";
             break;
         case 'strong-wind':
-            output = "wi-forecast-io-wind";
+            output = "wi-forecast-io-wind  text-danger";
+            clouds = " <b>Strong winds!</b>";
             break;
         case 'fog':
-            output = "wi-forecast-io-fog";
+            output = "wi-forecast-io-fog text-warning";
+            clouds = "<b>Caution:</b> Foggy";
             break;
         case 'cloudy':
             output = "wi-forecast-io-cloudy";
+            clouds = " <b>Clouds:</b> Cloudy.";
             break;
         case 'day-cloudy':
             output = "wi-forecast-io-partly-cloudy-day";
+            clouds = "<b>Clouds:</b> Cloudy day.";
             break;
         case 'partly-cloudy-day':
             output = "wi-forecast-io-partly-cloudy-day";
+            clouds = " <b>Clouds:</b> Partly cloudy.";
             break;
         case 'night-cloudy':
             output = "wi-forecast-io-partly-cloudy-night";
+            clouds = "<b>Clouds:</b> Cloudy night.";
             break;
         case 'hail':
-            output = "wi-forecast-io-hail";
+            output = "wi-forecast-io-hail text-danger";
+            clouds = " <b>Rain:</b> Hail";
             break;
         case 'thunderstorm':
-            output = "wi-forecast-io-thunderstorm";
+            output = "wi-forecast-io-thunderstorm text-danger";
+            clouds = "<b>Thunderstorm Warning</b>";
             break;
         case 'tornado':
-            output = "wi-forecast-io-tornado";
+            output = "wi-forecast-io-tornado text-danger";
+            clouds = "<b>TORNADO WARNING</b>"
             break;
         case 'default':
             output = "d-none";
             break;
     }
-    return "<i class=\"wi " + output + "\"></i>";
+    return "<i class=\"wi " + output + "\">"+ clouds +"</i>";
 }
 
 // *******************************************************
 // Converts UNIX Time Stamp to Human readable
 function timeConvert(unixTime) {
     var dateObject = new Date(unixTime * 1000);
-    return dateObject.toLocaleDateString();
+    var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    return dateObject.toLocaleDateString("en-US", options);
 }
 
 // Converts fahrenheit to celsius
@@ -171,24 +200,10 @@ function getCardinal(angle) {
 // ************* LISTENERS ******************************************************************************
 // If the Fahrenheit option is selected we pull the standard API results
 $("#mericanTemp").click(function () {
-    pullWeatherUS(0);
-    setTimeout(function(){
-        pullWeatherUS(1);
-    }, 1000);
-    setTimeout(function(){
-        pullWeatherUS(2);
-    }, 1000);
+
 });
 // If metric is selected we convert temperatures and wind to celsius and kilometers
 $("#metricTemp").click(function () {
-    pullWeatherMetric(0);
-    pullWeatherMetric(0);
-    setTimeout(function(){
-        pullWeatherMetric(1);
-    }, 1000);
-    setTimeout(function(){
-        pullWeatherMetric(2);
-    }, 1000);
 
 });
 
@@ -199,17 +214,18 @@ $("#metricTemp").click(function () {
 // TODAY'S FORECAST - [0]
 // Gets all the appropriate data to display the weather forecasts, adjust styling responsively.
 // DAILY temp high and low[x].. icon[ ] .. clouds:[ ] .. humidity[ ].. wind speed[ ] .. pressure[ ] ..
-function getMetaData() {
 
-    $.get(urlBuilder(lat, lng)).done(function (data) {
-        var tmpTime = data.daily.data[0].time;
-        $('#todayDate').empty().append(timeConvert(tmpTime))
-        if ($(window).width() > 1200) {
-            $('.wi').css('font-size', '3rem',);
-        }
 
-    });
+function cardBuilder(number) {
+
+    var getPath = data.daily.data[0];
+    var theDate = timeConvert(getPath.time);
+    var theSummary = getPath.summary;
+    return theDate;
+
+
 }
+
 
 // This is the Standard get without conversions then refreshes the box
 // TODO: make the weather api call and DOM all one funciton !!!!
@@ -217,40 +233,26 @@ function getMetaData() {
 // TODO: make the weather api call and DOM all one funciton !!!!
 // TODO: make the weather api call and DOM all one funciton !!!!
 
-function pullWeatherUS(i) {
-    var classDay = "#" + i + "-weatherBox";
-    $.get(urlBuilder(lat, lng)).done(function (data) {
-        console.log(data);
-        $(classDay).empty().append(data.daily.data[i].temperatureHigh.toFixed(0) + " | " +
-        data.daily.data[i].temperatureLow.toFixed(0) +
-        "<span>&#8457</span>" +
-        "<br>" +
-        iconPull(data.daily.data[i].icon) +
-        "<br>" +
-        getCardinal(data.daily.data[i].windbearing)
-            );
-    });
-    getMetaData();
-}
 
 // This calls the same as standard but with some fahrenheit to celsius conversions then refreshes the box
-function pullWeatherMetric(i) {
-    var classDay = "#" + i + "-weatherBox";
-    $.get(urlBuilder(lat, lng)).done(function (data) {
-        var hi = data.daily.data[i].temperatureHigh;
-        var low = data.daily.data[i].temperatureLow;
-        var windBear = data.daily.data[i].windbearing;
-        var tempInCelsiusHi = fToC(hi);
-        var tempInCelsiusLow = fToC(low);
-        $('#dayZero-weatherBox').empty().append(tempInCelsiusHi + " | " + tempInCelsiusLow + "<span>\xB0C</span>" +
-        "<br>" +
-        iconPull(data.daily.data[i].icon) +
-        "<br>" +
-        getCardinal(windBear)
-            );
-    });
-    getMetaData();
-}
+// function pullWeatherMetric(i) {
+//     var classDay = "#" + i + "-weatherBox";
+//     $.get(urlBuilder(lat, lng)).done(function (data) {
+//         var hi = data.daily.data[i].temperatureHigh;
+//         var low = data.daily.data[i].temperatureLow;
+//         var windBear = data.daily.data[i].windbearing;
+//         var tempInCelsiusHi = fToC(hi);
+//         var tempInCelsiusLow = fToC(low);
+//         $('#dayZero-weatherBox').empty().append(tempInCelsiusHi + " | " + tempInCelsiusLow + "<span>\xB0C</span>" +
+//         "<br>" +
+//         iconPull(data.daily.data[i].icon) +
+//         "<br>" +
+//         getCardinal(windBear)
+//             );
+//     });
+//     getMetaData();
+// }
+
 // TODO: make the weather api call and DOM all one funciton !!!!
 // TODO: make the weather api call and DOM all one funciton !!!!
 // TODO: make the weather api call and DOM all one funciton !!!!
