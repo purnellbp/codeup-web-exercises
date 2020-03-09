f9d4165b74ad534472f136364fdd89624f77030a
Brian Purnell <purnellbp@gmail.com>
2020-03-08 20:14:53 -0500

    weather map updats

--- js/darkSky.js
+++ js/darkSky.js
@@ -17,45 +17,71 @@ $('document').ready(function () {
             playSpeed: 0.2,
             path: 'img/weather-img/data.json' // the path to the animation json
         });
+
     }, 2000);
+
 });
-// *************************************************************************************************
 
+// $('#day0').on('
+// ******************************************************************************************************
+// ************* WEATHER BUILD **************************************************************************
 
+// TODAY'S FORECAST - [0]
+// Gets all the appropriate data to display the weather forecasts, adjust styling responsively.
+// DAILY temp high and low[x].. icon[ ] .. clouds:[ ] .. humidity[ ].. wind speed[ ] .. pressure[ ] ..
 // Call dark sky api
 var lat = 29.4383;
 var lng = -98.5031;
 var darkURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkysKey + "/" + lat + "," + lng;
 var tempSwitch = 0;
+
+
 // Forecast cards
 function buildCards() {
-
     $.get(darkURL)
         .done(function (data) {
             console.log(data);
+            // Config Dates and times
+            var timeDisplay = "";
+            var timeThen = timeConvert(data.daily.data[0].time);
+            var timeNow = new Date().getHours();
+            //var timeThen = new Date(data.daily.data[0].time).getHours();
+            console.log(timeNow);
+            console.log(timeThen);
+            // Temperature Unit Selection
+            // ID faraSelector and ID celsiusSelector | toggle
+            $('#unitSelector').click(function () {
+
+            });
+
             // Build Start
             for (var i = 0; i < data.daily.data.length; i++) {
                 var dayA = data.daily.data[i];
                 var thisCard = "#day" + i;
-                var tempHigh = dayA.temperatureHigh + "\xB0F";
-                var tempLow = dayA.temperatureLow + "\xB0F";
+                var dayDisplay = timeConvert(dayA.time).format('l');
+
+                // degrees stuff = "\xB0F"  "\xB0C"
+                var tempHigh = dayA.temperatureHigh.toFixed(0);
+                var tempLow = dayA.temperatureLow.toFixed(0);
                 if (tempSwitch === 1) {
-                    tempHigh = fToC(dayA.temperatureHigh) + "\xB0C";
-                    tempLow = fToC(dayA.temperatureLow) + "\xB0C";
+                    tempHigh =  fToC(dayA.temperatureHigh).toFixed(0);
+                    tempLow = fToC(dayA.temperatureLow).toFixed(0);
                 }
-                $(thisCard).append("<h5 class=\"card-title\">" + timeConvert(dayA.time))
-                    .append("</h5><p class=\"card-text\">" + dayA.summary + "</p>") // "blah blah throughout the day"
-                    .append("<ul class=\"list-group list-group-flush\">")
-                    .append("<li class=\"list-group-item text-primary\">" + tempHigh + " | " + tempLow + "</li>")
-                    .append("<li class=\"list-group-item\">" + iconPull(dayA.icon) + "|" + getCardinal(dayA.windBearing) + "</li>")
-                    .append("<li class=\"list-group-item\">Ves</li></ul>")
+                var summaryDisplay = iconPull(dayA.icon) + capitalizeFirstLetter(dayA.precipType);
+                var degreesSwitch = "<div><span id='unitSelector'><small id='faraSelector' class=' font-weight-bold'>\xB0F</small> | <small id='celsiusSelector'>\xB0C</small></span></div></div>"
+
+                // Card Build
+                $(thisCard)
+                    .append("")
 
 
-            }
-        });
+            } // end FOR loop
+        }); // End get.done
 }
-
 buildCards();
+
+
+
 // ****************************************************************************************************
 // MapBox
 // ****************************************************************************************************
@@ -101,77 +127,60 @@ function loadMapbox() {
 // clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, partly-cloudy-night
 function iconPull(iconName) {
     var output = "";
-    var clouds = "";
-    switch (iconName) {
+        switch (iconName) {
         case 'clear-day':
             output = "wi-forecast-io-clear-day";
-            clouds = " It's clear today!";
             break;
         case 'clear-night':
             output = "wi-forecast-io-clear-night";
-            clouds = " Clear night.";
             break;
         case  'rain':
             output = "wi-forecast-io-rain";
-            clouds = " <b>Rain</b> today";
             break;
         case  'snow':
             output = "wi-forecast-io-snow text-warning";
-            clouds = " <b>Snow</b> today.";
             break;
         case 'sleet':
             output = "wi-forecast-io-sleet  text-danger";
-            clouds = " <b>Rain:</b> Sleet";
             break;
         case 'strong-wind':
             output = "wi-forecast-io-wind  text-danger";
-            clouds = " <b>Strong winds!</b>";
             break;
         case 'fog':
             output = "wi-forecast-io-fog text-warning";
-            clouds = "<b>Caution:</b> Foggy";
             break;
         case 'cloudy':
             output = "wi-forecast-io-cloudy";
-            clouds = " <b>Clouds:</b> Cloudy.";
             break;
         case 'day-cloudy':
             output = "wi-forecast-io-partly-cloudy-day";
-            clouds = "<b>Clouds:</b> Cloudy day.";
             break;
         case 'partly-cloudy-day':
             output = "wi-forecast-io-partly-cloudy-day";
-            clouds = " <b>Clouds:</b> Partly cloudy.";
             break;
         case 'night-cloudy':
             output = "wi-forecast-io-partly-cloudy-night";
-            clouds = "<b>Clouds:</b> Cloudy night.";
             break;
         case 'hail':
             output = "wi-forecast-io-hail text-danger";
-            clouds = " <b>Rain:</b> Hail";
             break;
         case 'thunderstorm':
             output = "wi-forecast-io-thunderstorm text-danger";
-            clouds = "<b>Thunderstorm Warning</b>";
             break;
         case 'tornado':
             output = "wi-forecast-io-tornado text-danger";
-            clouds = "<b>TORNADO WARNING</b>"
             break;
         case 'default':
             output = "d-none";
             break;
     }
-    return "<i class=\"wi " + output + "\">"+ clouds +"</i>";
+    return "<i class=\"wi " + output + "\"></i>";
 }
 
-// *******************************************************
 // Converts UNIX Time Stamp to Human readable
+// Date handling borrowed from https://github.com/jacwright/date.format
 function timeConvert(unixTime) {
-    var dateObject = new Date(unixTime * 1000);
-    var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-    return dateObject.toLocaleDateString("en-US", options);
+   return new Date(unixTime * 1000);
 }
 
 // Converts fahrenheit to celsius
@@ -196,6 +205,11 @@ function getCardinal(angle) {
                                 : "<i class=\"wi wi-wind wi-from-nw\"></i><span>NW</span>";
 }
 
+// Capitalize first letter
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 // ******************************************************************************************************
 // ************* LISTENERS ******************************************************************************
 // If the Fahrenheit option is selected we pull the standard API results
@@ -208,57 +222,6 @@ $("#metricTemp").click(function () {
 });
 
 
-// ******************************************************************************************************
-// ************* WEATHER BUILD **************************************************************************
 
-// TODAY'S FORECAST - [0]
-// Gets all the appropriate data to display the weather forecasts, adjust styling responsively.
-// DAILY temp high and low[x].. icon[ ] .. clouds:[ ] .. humidity[ ].. wind speed[ ] .. pressure[ ] ..
-
-
-function cardBuilder(number) {
-
-    var getPath = data.daily.data[0];
-    var theDate = timeConvert(getPath.time);
-    var theSummary = getPath.summary;
-    return theDate;
-
-
-}
-
-
-// This is the Standard get without conversions then refreshes the box
-// TODO: make the weather api call and DOM all one funciton !!!!
-// TODO: make the weather api call and DOM all one funciton !!!!
-// TODO: make the weather api call and DOM all one funciton !!!!
-// TODO: make the weather api call and DOM all one funciton !!!!
-
-
-// This calls the same as standard but with some fahrenheit to celsius conversions then refreshes the box
-// function pullWeatherMetric(i) {
-//     var classDay = "#" + i + "-weatherBox";
-//     $.get(urlBuilder(lat, lng)).done(function (data) {
-//         var hi = data.daily.data[i].temperatureHigh;
-//         var low = data.daily.data[i].temperatureLow;
-//         var windBear = data.daily.data[i].windbearing;
-//         var tempInCelsiusHi = fToC(hi);
-//         var tempInCelsiusLow = fToC(low);
-//         $('#dayZero-weatherBox').empty().append(tempInCelsiusHi + " | " + tempInCelsiusLow + "<span>\xB0C</span>" +
-//         "<br>" +
-//         iconPull(data.daily.data[i].icon) +
-//         "<br>" +
-//         getCardinal(windBear)
-//             );
-//     });
-//     getMetaData();
-// }
-
-// TODO: make the weather api call and DOM all one funciton !!!!
-// TODO: make the weather api call and DOM all one funciton !!!!
-// TODO: make the weather api call and DOM all one funciton !!!!
-// TODO: make the weather api call and DOM all one funciton !!!!
-
-
-// Sandboxing
 
 
